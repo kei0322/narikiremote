2d39bd0fd7ef46ed711de5a07df42846c20da32b
RikuHattori <rikuroku0308@gmail.com>
2021-09-08 17:43:47 +0900

    3ターン毎に最終結果パネルを表示するスクリプト、そこから待機画面に戻るボタン、スクリプトの追加、

---
+++ Assets/scripts/Saishuu_panelclose.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saishuu_panelclose : MonoBehaviour
+{
+    //2021.9.7　服部
+    [SerializeField]
+    public GameObject role_change_canvas;
+    public GameObject theme_change_canvas;
+    public GameObject Saishuu_Panel;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    public void panelclose()
+    {
+        Saishuu_Panel.SetActive(false);
+        role_change_canvas.gameObject.SetActive(true);
+        theme_change_canvas.gameObject.SetActive(true);
+
+    }
+}
