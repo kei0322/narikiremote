3b3718804c7b9c3565bf2f7d12733461e2572f74
kei0322 <ke11plus11.0322@gmail.com>
2021-09-18 16:54:17 +0900

    動画用プログラムの場所を戻した

--- Assets/scripts/Question/theme_change.cs
+++ Assets/scripts/Question/theme_change.cs
@@ -55,10 +55,10 @@ public class theme_change : MonoBehaviourPunCallbacks
                 theme_button.gameObject.SetActive(false);
             }
             if (v.ep) theme_canvas.gameObject.SetActive(false);
-            //int theme_rnd = (int)Random.Range(0.0f, 2.0f);//カテゴリーをランダム
-            //if (theme_rnd == 0) vegetable();
-            //if (theme_rnd == 1) animal();
-            vegetable();
+            int theme_rnd = (int)Random.Range(0.0f, 3.0f);//カテゴリーをランダム
+            if (theme_rnd == 0) vegetable();
+            if (theme_rnd == 1) animal();
+            if (theme_rnd == 2) sports();
         }
 
         Debug.Log("参加者数 : " + pc.PlayerCount + "/最大人数 : " + pc.MaxPlayers);
@@ -76,6 +76,13 @@ public class theme_change : MonoBehaviourPunCallbacks
     {
         //v.theme = "chaos";
         photonView.RPC(nameof(theme_set), RpcTarget.All, "animal");
+        photonView.RPC(nameof(erase_panel), RpcTarget.All);
+    }
+
+    public void sports()
+    {
+        //v.theme = "chaos";
+        photonView.RPC(nameof(theme_set), RpcTarget.All, "sports");
         photonView.RPC(nameof(erase_panel), RpcTarget.All);
     }
 
