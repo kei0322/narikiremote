705985c0584fa311e79e35754314ec29f9610ebf
TaigaIwashita <77519967+TaigaIwashita@users.noreply.github.com>
2021-09-20 22:20:00 +0900

    幕開け

---
+++ Assets/Taiga_answer/Sound/GradeObjectSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradeObjectSound : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip Fanfale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        v.grade_se_fg = true;
+        v.se_timer = 3.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v.se_timer -= Time.deltaTime;
+        if(v.se_timer < 0 && v.grade_se_fg)
+        {
+            v.grade_se_fg = false;
+            audioSource.PlayOneShot(Fanfale);
+        }
+    }
+}
